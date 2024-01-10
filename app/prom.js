ca5e86cef1aaf573ca4a215b793295da317ab894
k03mad <k03.mad@gmail.com>
2024-01-10 19:32:33 +0300

    update deps

--- app/prom.js
+++ app/prom.js
@@ -5,7 +5,7 @@ import client from 'prom-client';
 
 import env from '../env.js';
 
-import {errorStack, errorText} from './helpers/colors.js';
+import {errorText} from './helpers/colors.js';
 import {logPlainError} from './helpers/logging.js';
 import {packageJson} from './helpers/parse.js';
 import * as metrics from './metrics/_index.js';
@@ -35,7 +35,6 @@ Object
                     logPlainError([
                         `${getDateYMDHMS()} [${name}]`,
                         errorText(err),
-                        errorStack(err.stack),
                     ]);
                 }
             },
