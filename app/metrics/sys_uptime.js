44e98d30f87c471fa203688aacd377d31b880ed4
k03mad <k03.mad@gmail.com>
2024-01-10 04:55:03 +0300

    pretty

--- app/metrics/sys_uptime.js
+++ app/metrics/sys_uptime.js
@@ -2,6 +2,7 @@ import {getCurrentFilename} from '../helpers/paths.js';
 import {run} from '../helpers/shell.js';
 
 const CMD = 'uptime -p';
+const re = /^up\s+/;
 
 export default {
     name: getCurrentFilename(import.meta.url),
@@ -12,6 +13,6 @@ export default {
         ctx.reset();
 
         const stdout = await run(CMD);
-        ctx.labels(stdout.replace('up ', '')).set(1);
+        ctx.labels(stdout.replace(re, '')).set(1);
     },
 };
