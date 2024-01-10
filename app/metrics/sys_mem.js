d97732f7ab2a961be473c30c44ad71a998d89d04
k03mad <k03.mad@gmail.com>
2024-01-11 01:10:32 +0300

    exclude metrics

--- app/metrics/sys_mem.js
+++ app/metrics/sys_mem.js
@@ -4,6 +4,8 @@ import {run} from '../helpers/shell.js';
 const CMD = 'cat /proc/meminfo';
 const UNIT = 'kB';
 
+const NAMES_EXCLUDE = new Set(['VmallocTotal']);
+
 export default {
     name: getCurrentFilename(import.meta.url),
     help: CMD,
@@ -19,7 +21,10 @@ export default {
             const cells = row.split(/\s+/);
             const name = cells[0].replace(':', '');
 
-            if (cells[2] === UNIT) {
+            if (
+                cells[2] === UNIT
+                && !NAMES_EXCLUDE.has(name)
+            ) {
                 ctx.labels(name).set(Number(cells[1]));
             }
         });
