8bccd60e2e3cbc8c9101488a19a0cc633176272a
k03mad <k03.mad@gmail.com>
2024-01-11 14:11:17 +0300

    one more range

---
+++ app/metrics/sys_cpu_range_avg_t.js
@@ -0,0 +1,25 @@
+import {getCurrentFilename} from '../helpers/paths.js';
+import {run} from '../helpers/shell.js';
+
+const CMD = 'mpstat 1 3';
+
+export default {
+    name: getCurrentFilename(import.meta.url),
+    help: CMD,
+    labelNames: ['type'],
+
+    async collect(ctx) {
+        ctx.reset();
+
+        const stdout = await run(CMD);
+        const table = stdout.split('\n');
+
+        const headers = table[2].split(/\s+/);
+
+        table[6].split(/\s+/).forEach((cell, i) => {
+            if (i >= 2) {
+                ctx.labels(headers[i].replace('%', '')).set(Number(cell.replace(',', '')));
+            }
+        });
+    },
+};
