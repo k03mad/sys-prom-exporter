24a683758528637d46dfefe872968067c5925e01
k03mad <k03.mad@gmail.com>
2024-01-11 05:28:06 +0300

    update cpu

--- app/metrics/sys_cpu.js
+++ app/metrics/sys_cpu.js
@@ -1,39 +1,31 @@
 import {getCurrentFilename} from '../helpers/paths.js';
 import {run} from '../helpers/shell.js';
 
-const CMD = 'top -ibn1';
-
-const RE = {
-    cpu: /(?<User>[\d,]+)\s+us,\s+(?<System>[\d,]+)\s+sy,\s+(?<Nice>[\d,]+)\s+ni,\s+(?<Idle>[\d,]+)\s+id,\s+(?<IoWait>[\d,]+)\s+wa,\s+(?<HardIrq>[\d,]+)\s+hi,\s+(?<SoftIrq>[\d,]+)\s+si,\s+(?<Steal>[\d,]+)\s+st/,
-    load: /load average:\s+(?<_1m>[\d,]+),\s+(?<_5m>[\d,]+),\s+(?<_15m>[\d,]+)/,
-    uptime: /up\s(.+),\s+\d+\s+user/,
-};
+const CMD = 'mpstat';
 
 export default {
     name: getCurrentFilename(import.meta.url),
     help: CMD,
-    labelNames: [
-        'name',
-        'type',
-    ],
+    labelNames: ['type'],
 
     async collect(ctx) {
         ctx.reset();
 
         const stdout = await run(CMD);
+        const table = stdout.split('\n');
 
-        const cpuGroups = stdout.match(RE.cpu).groups;
-        const loadGroups = stdout.match(RE.load).groups;
-        const uptime = stdout.match(RE.uptime)[1];
+        const headers = table[2]
+            .split(/\s+/)
+            .filter(elem => elem.includes('%'))
+            .map(elem => elem.replace('%', ''));
 
-        Object.entries(cpuGroups).forEach(([type, num]) => {
-            ctx.labels('cpu', type).set(Number(num.replace(',', '.')));
+        const values = table[3]
+            .split(/\s+/)
+            .filter(elem => elem.includes(','))
+            .map(elem => Number(elem.replace(',', '.')));
+
+        headers.forEach((header, i) => {
+            ctx.labels(header).set(values[i]);
         });
-
-        Object.entries(loadGroups).forEach(([type, num]) => {
-            ctx.labels('load', type).set(Number(num.replace(',', '.')));
-        });
-
-        ctx.labels('uptime', uptime).set(1);
     },
 };
