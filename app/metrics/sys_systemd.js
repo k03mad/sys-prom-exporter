8fe8dfed046f6ec00931fe84e85c57ea39b4f008
k03mad <k03.mad@gmail.com>
2024-01-14 00:13:35 +0300

    merge systemd

--- app/metrics/sys_systemd.js
+++ app/metrics/sys_systemd.js
@@ -3,18 +3,28 @@ import {getCurrentFilename} from '../helpers/paths.js';
 import {run} from '../helpers/shell.js';
 
 const CMD = 'systemctl list-units --all';
+const CMD_FAILED = 'systemctl list-units --failed';
+
 const FIND_INCLUDES = '.';
 
 export default {
     name: getCurrentFilename(import.meta.url),
     help: CMD,
-    labelNames: ['type'],
+    labelNames: [
+        'name',
+        'type',
+    ],
 
     async collect(ctx) {
         ctx.reset();
 
-        const stdout = await run(CMD);
+        const [stdout, stdoutFailed] = await Promise.all([
+            run(CMD),
+            run(CMD_FAILED),
+        ]);
+
         const table = stdout.split('\n');
+        const tableFailed = stdoutFailed.split('\n');
 
         const counters = {};
 
@@ -32,8 +42,20 @@ export default {
             }
         });
 
+        tableFailed.forEach((row, i) => {
+            // header
+            if (i > 0) {
+                const cells = row.split(/\s+/);
+                const service = cells[1];
+
+                if (service?.includes(FIND_INCLUDES)) {
+                    ctx.labels('failed', service).set(1);
+                }
+            }
+        });
+
         Object.entries(counters).forEach(([type, value]) => {
-            ctx.labels(type).set(value);
+            ctx.labels('count', type).set(value);
         });
     },
 };
