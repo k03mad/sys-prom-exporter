64c1a7fa805b7a7b4c22d525cb4460f42aad80b0
k03mad <k03.mad@gmail.com>
2024-12-24 13:09:11 +0300

    add cpu temp

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 npm run setup
 
 # start app
-npm run start --port=11000 --turnoff=sys_ufw,sys_wg
+npm run start --port=11000 --turnoff=sys_temp,sys_ufw,sys_wg
 # or with envs
-SYS_EXPORTER_PORT=11000 SYS_EXPORTER_METRICS_TURN_OFF=sys_ufw,sys_wg npm run start
+SYS_EXPORTER_PORT=11000 SYS_EXPORTER_METRICS_TURN_OFF=sys_temp,sys_ufw,sys_wg npm run start
 ```
 
 [grafana-dashboards](https://github.com/k03mad/grafana-dashboards/tree/master/export)
---
+++ app/metrics/sys_temp.js
@@ -0,0 +1,17 @@
+import {getCurrentFilename} from '../helpers/paths.js';
+import {run} from '../helpers/shell.js';
+
+const CMD = 'cat /sys/class/thermal/thermal_zone0/temp';
+
+export default {
+    name: getCurrentFilename(import.meta.url),
+    help: CMD,
+    labelNames: ['type'],
+
+    async collect(ctx) {
+        ctx.reset();
+
+        const cpuTemp = await run(CMD);
+        ctx.labels('cpu').set(Number(cpuTemp.trim()));
+    },
+};
