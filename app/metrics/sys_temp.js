0ea44bd3417e51de2790c654d495a860c61e445e
k03mad <k03.mad@gmail.com>
2025-02-17 01:10:48 +0300

    fix all zones

--- app/metrics/sys_temp.js
+++ app/metrics/sys_temp.js
@@ -2,18 +2,18 @@ import {getCurrentFilename} from '../helpers/paths.js';
 import {run} from '../helpers/shell.js';
 
 const THERMAL_ZONES = [0, 1, 2, 3];
-const CMD = zone => `cat /sys/class/thermal/thermal_zone${zone}/temp`;
+const cmd = zone => `cat /sys/class/thermal/thermal_zone${zone}/temp`;
 
 export default {
     name: getCurrentFilename(import.meta.url),
-    help: CMD,
+    help: cmd(0),
     labelNames: ['type', 'zone'],
 
     async collect(ctx) {
         ctx.reset();
 
         await Promise.all(THERMAL_ZONES.map(async zone => {
-            const cpuTemp = await run(CMD);
+            const cpuTemp = await run(cmd(zone));
             ctx.labels('cpu', zone).set(Number(cpuTemp.trim()) / 1000);
         }));
     },
