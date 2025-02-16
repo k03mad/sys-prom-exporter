6e145524ace69ef8c05aa0df6185bc1fa20d4555
k03mad <k03.mad@gmail.com>
2025-02-17 01:09:34 +0300

    add all zones

--- app/metrics/sys_temp.js
+++ app/metrics/sys_temp.js
@@ -1,17 +1,20 @@
 import {getCurrentFilename} from '../helpers/paths.js';
 import {run} from '../helpers/shell.js';
 
-const CMD = 'cat /sys/class/thermal/thermal_zone0/temp';
+const THERMAL_ZONES = [0, 1, 2, 3];
+const CMD = zone => `cat /sys/class/thermal/thermal_zone${zone}/temp`;
 
 export default {
     name: getCurrentFilename(import.meta.url),
     help: CMD,
-    labelNames: ['type'],
+    labelNames: ['type', 'zone'],
 
     async collect(ctx) {
         ctx.reset();
 
-        const cpuTemp = await run(CMD);
-        ctx.labels('cpu').set(Number(cpuTemp.trim()) / 1000);
+        await Promise.all(THERMAL_ZONES.map(async zone => {
+            const cpuTemp = await run(CMD);
+            ctx.labels('cpu', zone).set(Number(cpuTemp.trim()) / 1000);
+        }));
     },
 };
